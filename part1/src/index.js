2a8a652ffc43f6ea597530a0ed4b689b0b79bae8
moon-lee <lee.moonkyu@gmail.com>
2019-10-14 12:30:01 +1000

    exercise part1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-    <div>
-        <p>Hello World!!</p>
-    </div>
-)
+const Hello = (prop) => {
+    return (
+        <div>
+            <p>Hello {prop.name}, you are {prop.age} years old</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const name = "Peter"
+    const age = 10
+    return (
+        <>
+            <h1>Greetings</h1>
+            <Hello name="Moon Lee" age={20 + 10} />
+            <Hello name={name} age={age} />
+            <Footer />
+        </>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            Greeting app created by
+            <a href="https://github.com/moon-lee">Moon Lee</a>
+        </div>
+
+    )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
