59410122299db6a3134e761a7f8e5c9bfc0a1691
moon-lee <lee.moonkyu@gmail.com>
2019-10-25 11:37:22 +1000

    stateful component

--- part1/part1-example/src/index.js
+++ part1/part1-example/src/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const App = (props) => {
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    const [counter, setCounter] = useState(0)
+
+    setTimeout(
+        () => setCounter(counter + 1), 1000
+    )
+
+    console.log('rendering....', counter)
+    return (
+        <div>{counter}</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
 
