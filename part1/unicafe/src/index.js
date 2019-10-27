049e9c6a046256eaca02a35a0e4f7bdb1355148c
moon-lee <lee.moonkyu@gmail.com>
2019-10-27 21:22:10 +1000

    unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,28 +1,67 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+
+const Button = ({onClick, text}) =>  <button onClick={onClick}>{text}</button>
+
 const App = () => {
+
+    const [good, setGood] = useState(0)
+    const [neutral, setNeural] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => setGood(good+1)
+    const handleNeutralClick = () => setNeural(neutral+1)
+    const handleBadClick = () => setBad(bad+1)
+
+    const all = good+neutral+bad
+    const average = () => {
+        if (all === 0) return 0
+        return (
+            (good*1 + neutral*0 + bad*-1) /all
+        )
+    }
+    const positive = () => {
+        if (all === 0) return 0
+        return (
+            good/all*100
+        )
+    }
 
     return (
         <div>
             <h1>Give Feedback</h1>
-            <button>good</button>
-            <button>neutral</button>
-            <button>bad</button>
+            <Button onClick={handleGoodClick} text='good' />
+            <Button onClick={handleNeutralClick} text='neutral' />
+            <Button onClick={handleBadClick} text='bad' />
             <h1>Statistic</h1>
             <table>
                 <tr>
                     <td>good</td>
-                    <td>2</td>
+                    <td>{good}</td>
                 </tr>
                 <tr>
                     <td>neutral</td>
-                    <td>3</td>
+                    <td>{neutral}</td>
                 </tr>
                 <tr>
                     <td>bad</td>
-                    <td>4</td>
+                    <td>{bad}</td>
                 </tr>
+                <tr>
+                    <td>all</td>
+                    <td>{all}</td>
+                </tr>
+                <tr>
+                    <td>average</td>
+                    <td>{average()}</td>
+                </tr>
+                <tr>
+                    <td>positive</td>
+                    <td>{positive()} %</td>
+                </tr>
+
+
             </table>
         </div>
     )
