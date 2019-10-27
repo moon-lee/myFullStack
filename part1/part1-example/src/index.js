5a188bb882f8d37c00c54eb04ff5c935b43c16ef
moon-lee <lee.moonkyu@gmail.com>
2019-10-27 16:41:28 +1000

    Debugging

--- part1/part1-example/src/index.js
+++ part1/part1-example/src/index.js
@@ -1,27 +1,57 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
 
-    const [counter, setCounter] = useState(0)
-
-    const setToValue = (value) => () => setCounter(value)
-  
     return (
-        <>
-            <Display counter={counter}/>
-            <Button onClick={setToValue(counter + 1)} text='plus'/>
-            <Button onClick={setToValue(counter - 1)} text='minus'/>
-            <Button onClick={setToValue(0)} text='zero'/>
-        </>
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
     )
 }
 
-const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({onClick, text}) =>  <button onClick={onClick}>{text}</button>
+ 
+const App = () => {
 
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0) 
+    const [allClicks, setAll] = useState([])
 
+    // const [clicks, setClicks] = useState({left: 0, right: 0})
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left+1)
+        // setClicks({...clicks, left: clicks.left+1 })
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right+1)
+        // setClicks({ ...clicks, right: clicks.right+1 })
+    }
+
+    return (
+        <>
+            <div>
+                {left}
+                <Button onClick={handleLeftClick} text='Left' />
+                <Button onClick={handleRightClick} text='Right' />
+                {right}
+                <History allClicks={allClicks} />
+            </div>
+        </>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
