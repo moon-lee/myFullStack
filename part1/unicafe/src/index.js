2aa2bcbc7c5aad51d14f841b897c19dd3f784709
moon-lee <lee.moonkyu@gmail.com>
2019-10-27 20:31:40 +1000

    unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+
+const App = () => {
+
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+            <button>good</button>
+            <button>neutral</button>
+            <button>bad</button>
+            <h1>Statistic</h1>
+            <table>
+                <tr>
+                    <td>good</td>
+                    <td>2</td>
+                </tr>
+                <tr>
+                    <td>neutral</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>bad</td>
+                    <td>4</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
