b80a6267347d6ad7bc9104257755f5f84f10102d
moon-lee <lee.moonkyu@gmail.com>
2020-01-13 00:27:11 +1000

    part2 example done

--- part2/part2-example/src/index.js
+++ part2/part2-example/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 const notes = [
     {
@@ -19,24 +20,9 @@ const notes = [
         content: 'GET and POST are the most important methods of HTTP protocal',
         date: '2019-12-31T15:22:14.015Z',
         important: true
-    },
+    }
 ]
 
-const App = (props) => {
-
-    const { notes } = props
-    const rows = () => notes.map(note => <li key={note.id}>{note.content}</li>)
-
-    return (
-        <div>
-            <h1>Notes</h1>
-            <ul>
-                {rows()}
-            </ul>
-        </div>
-    )
-}
 
 ReactDOM.render(<App notes={notes} />, document.getElementById('root'));
-
 
