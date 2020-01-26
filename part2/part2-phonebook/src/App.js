7f3ec54bf6726e23b528c1985e0898997ce2b74c
moon-lee <lee.moonkyu@gmail.com>
2020-01-26 16:29:36 +1000

    part2-8 phonebook done

--- part2/part2-phonebook/src/App.js
+++ part2/part2-phonebook/src/App.js
@@ -3,56 +3,92 @@ import React, { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     {
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      number: '040-123456'
     },
     {
-      name: 'Ada Lovelace'
+      name: 'Ada Lovelace',
+      number: '39-44-5323523'
+    },
+    {
+      name: 'Dan Abramov',
+      number: '12-43-234345'
+    },
+    {
+      name: 'Mary Poppendieck',
+      number: '39-23-6423122'
     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const listPerson = () => persons.map(person =>
-    <Person
-      key={person.name}
-      person={person}
-    />
-  )
 
   const addNew = (e) => {
     e.preventDefault()
-    console.log('Add button click', e.target)
-    const nameObject = {
-      name: newName
+    const isExist = persons.map(person => person.name).includes(newName)
+
+    if (!isExist) {
+      const nameObject = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(nameObject))
+    } else {
+      alert(`${newName} is already added to phonebook`)
     }
-    setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
 
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+        
+      <h3>Add a new person</h3>
       <form onSubmit={addNew}>
         <div>
-          name: <input type="text" value={newName} onChange={handleNameChange}/>
+          name: <input type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input type="text" value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <div>{listPerson()}</div>
+      <h3>Numbers</h3>
+        <Persons persons={persons} />
     </div>
   )
 }
 
 const Person = ({ person }) => {
   return (
-    <div>{person.name}</div>
+    <div>{person.name} {person.number}</div>
   )
 }
+
+const Persons = ({ persons }) => {
+  const listPerson = () => persons.map(person =>
+    <Person
+      key={person.name}
+      person={person}
+    />
+  )
+  return (
+    <div>{listPerson()}</div>
+  )
+}
+
+
 
 export default App
