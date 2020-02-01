3c152926c8324ff24639812c0fb516077d7280d9
moon-lee <lee.moonkyu@gmail.com>
2020-02-01 22:06:28 +1000

    part2-c example done

--- part2/part2-example/src/index.js
+++ part2/part2-example/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+// import axios from 'axios'
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-12-31T15:20:56.961Z',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only javascript',
-        date: '2019-12-31T15:20:40.270Z',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocal',
-        date: '2019-12-31T15:22:14.015Z',
-        important: true
-    }
-]
+// axios.get('http://localhost:3001/notes').then(response => {
+//         const notes = response.data
+//         console.log('notes',notes)
 
+//         ReactDOM.render(<App notes={notes} />, document.getElementById('root'));
 
-ReactDOM.render(<App notes={notes} />, document.getElementById('root'));
+// })
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
